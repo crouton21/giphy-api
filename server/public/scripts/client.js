03cffe3a043e3ff27851555f03d579516b7a5881
Jacob Albright <jacob.albright23@gmail.com>
2018-02-23 11:55:01 -0600

    search function is functional: we get a bunch of gif objects, like cats

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,26 @@
 const app = angular.module('gifApp', []);
-const baseURL= 'http://api.giphy.com/v1/gifs';
+const baseURL= 'http://api.giphy.com/v1/gifs/';
 const apiKey= '&api_key=0rj09zTLmHbERPILRy44muwCAFJwgyCZ';
 
 const gifSearchController = app.controller('GifSearchController', ['$http', gifSearch])
 function gifSearch($http){
     let self = this;
     self.searchIn = '';
+    self.searchResults= [ ];
+
+    self.searchGif = function(searchString){
+        $http({
+            method: 'GET',
+            url: baseURL + 'search?q=' + searchString + apiKey
+        }).then(function(response){
+            self.searchResults = response.data.data;
+            console.log(response);
+            
+            console.log(self.searchResults);
+        }).catch(function(error){
+        console.log(error, 'WOMP');
+        })
+    }
 }
 
 const randomGifController = app.controller('RandomGifController', ['$http', gifRandom])
